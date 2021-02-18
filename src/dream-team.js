1453b9cae28088be52dd4dca2b7e343f0526076f
Карастелева Наталья Николаевна <Nnkarasteleva@ac.atlantconsult.com>
2021-02-18 19:43:50 +0300

    feat: second commit

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members ) {
+  var arr = [];
+  for(var i in members){
+    if(typeof(members[i] == "string")){
+      arr.push(members[i][0].toUpperCase());
+    }
+  }
+  arr.sort();
+  return arr.join('');
 };
