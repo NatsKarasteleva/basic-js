57953cfa95703aed93f836fb26ef2ccbdf8ea456
Natallia Karastsialiova <nats03121998@mail.ru>
2021-02-18 22:54:31 +0300

    feat: carbonDate

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+var l = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  sampleActivity = Number.parseInt(sampleActivity);
+  if(typeof(sampleActivity) != "string"){
+    return false;
+  }else{
+    var k = l/HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity)/k);
+  }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw "THROW";
+  }
+  var disNext = "--discard-next";
+  var disPrev = "--discard-prev";
+  var doubleNext = "--double-next";
+  var doublePrev = "--double-prev";
+  var res = [];
+  var disNextFlag = false;
+  var doubleNextFlag = false;
+  var globPrev;
+  for(var i in arr){
+    
+    if(disNextFlag){
+      disNextFlag = false;
+      continue;
+    }else if(doubleNextFlag){
+      res.push(arr[i]);
+      res.push(arr[i]);
+      doubleNextFlag = false;
+    }else if(arr[i] == disNext){
+      disNextFlag = true;
+    }else if(arr[i] == disPrev){
+        if(res.length != 0 && res[res.length - 1] == arr[i-1]){
+        res.pop();
+    }
+    }else if(arr[i] == doubleNext){
+      doubleNextFlag = true;
+    }else if(arr[i] == doublePrev){
+      if(res.length > 0 && globPrev != res[res.length - 1]){
+        res.push(arr[i-1]);
+      }
+    }else{
+      res.push(arr[i]);
+      globPrev = arr[i];
+    }
+  }
+  return res;
 };
