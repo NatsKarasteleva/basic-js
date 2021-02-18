f3df3f7ea8fa7f47f9dd7ba7cfd36325e427f861
Natallia Karastsialiova <nats03121998@mail.ru>
2021-02-18 20:26:59 +0300

    5 commit

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,18 @@
 module.exports = function createDreamTeam(members ) {
+  if(!Array.isArray(members)){
+    return false;
+  }
   var arr = [];
   for(var i in members){
-    if(typeof(members[i] === "string")){
-      arr.push(members[i][0].toUpperCase());
+    if(typeof(members[i]) === "string"){
+      var t = members[i].trim();
+      arr.push(t[0].toUpperCase());
     }
   }
   arr.sort();
   var res = arr.join('')
   if(res.length == 0){
-    return null;
+    return false;
   }
   return res;
 };
