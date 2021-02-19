e26393bed6315c048e12da8c07fa6527cf84235f
Карастелева Наталья Николаевна <Nnkarasteleva@ac.atlantconsult.com>
2021-02-19 15:38:43 +0300

    feat: calculate Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+  var turns = Math.pow(2, disksNumber) - 1;
+  var turnsTime = Math.floor(turns*3600/turnsSpeed);
+  var res = {};
+  res.turns = turns;
+  res.seconds = turnsTime;
+  return res;
 };
