95da8d66f9fa99f5f5395e293a9805bd1bc0d506
Natallia Karastsialiova <nats03121998@mail.ru>
2021-02-23 17:08:04 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  var res = "";
+  if(options.repeatTimes != undefined){
+  for (let index = 0; index < Number.parseInt(options.repeatTimes); index++) {
+    if(index != 0){
+      if(options.separator != undefined){
+        res+=options.separator;
+      }else{
+        res+='+';
+      }
+    }
+    res += str;
+    if(options.additionRepeatTimes != undefined){
+    for (let i = 0; i < Number.parseInt(options.additionRepeatTimes); i++) {
+      res+=options.addition;
+      if(i+1 != options.additionRepeatTimes){
+        
+        if(options.additionSeparator != undefined){
+          res+=options.additionSeparator;
+        }else{
+          res+='|';
+        }
+      }
+    }
+    }
+  }
+}else{
+  res+= str + options.addition;
+}
+  return res;
 };
   
